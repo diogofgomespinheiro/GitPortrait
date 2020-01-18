0940ccf631e28d4841e33df2ef754fdefa61d347
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2020-01-18 17:44:34 +0000

    build: added chart.js, randomcolor, lottie libraries

--- pages/index.js
+++ pages/index.js
@@ -8,24 +8,39 @@ import { Container, Title, Input } from "../styles/index";
 const Index = () => {
   const [username, setUsername] = useState("");
 
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  };
+
   const handleChange = e => {
     setUsername(e.target.value);
-  }
+  };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     Router.push({
       pathname: "/user",
       query: { username }
     });
-  }
+  };
 
   return (
     <Container>
       <div>
+        <Lottie options={defaultOptions} height={200} width={200} />
         <Title>Check your github profile</Title>
         <form onSubmit={handleSubmit}>
-          <Input type="text" placeholder="Username" value={username} onChange={handleChange} />
+          <Input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleChange}
+          />
         </form>
       </div>
     </Container>
